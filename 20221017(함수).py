b3c578867ac8b57d175f0f6281f407344677543a
kms19910502 <audtjr37@gmail.com>
2022-10-21 09:59:03 +0900

    branch test

--- 20221017(함수).py
+++
@@ -1,7 +0,0 @@
-def abc(a,b):
-    print(f'{a}+{b}={a+b}')
-    print(f'{a}-{10}={a-b}')
-    print(f'{a}*{b}={a*b}')
-    print(f'{a}/{b}={a/b}')
-
-abc(5,10)
--- 20221017-3(for문 구구단).py
+++
@@ -1,6 +0,0 @@
-a = int(input('입력해 주세요: '))
-
-for n in range(1,10): 
-    #range=특정 구간의 숫자 범위를 만들어 주는 함수
-    
-    print(f'{a}*{n}={a*n}')
